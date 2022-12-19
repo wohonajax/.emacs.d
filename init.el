f021f9007e713db02c868b216b308be5b1810f30
Hunter Chandler <wohonajax@gmail.com>
2022-12-19 13:14:54 -0600

    Add files via upload

--- init.el
+++ init.el
@@ -5,21 +5,50 @@
 ;;;; ------------------------------
 
 
-(defun load-init (file)
-  "Loads the specified init file."
-  (load (concat "~/.emacs.d/inits/"
-		(downcase (symbol-name file))
-		".el")))
 
-(defun load-inits (file-list)
-  "Loads the specified init files."
-  (mapc #'load-init file-list))
+(require 'package)
 
-(load-inits '(misc customize modes lisp keys))
+(mapc (lambda (x)
+        (add-to-list 'package-archives x t))
+      '(("gnu" . "http://elpa.gnu.org/packages/")
+        ("elpy" . "https://jorgenschaefer.github.io/packages/")
+        ("melpa-stable" . "https://stable.melpa.org/packages/")
+        ("melpa" . "https://melpa.org/packages/")))
+
+(package-initialize)
+
+(defun add-something-to-mode-hooks (mode-list something)
+  "Helper function to add a callback to multiple hooks."
+  (dolist (mode mode-list)
+    (add-hook (intern (concat (symbol-name mode) "-mode-hook")) something)))
+(byte-compile-fdefinition 'add-something-to-mode-hooks nil)
+
+(defun byte-compile-maybe-init (filename)
+  (if (string-prefix-p "~/.emacs.d/inits/" filename)
+      (concat (subseq filename 0 15) "elcs/"
+              (subseq filename 17) "c")
+      (if (string-prefix-p "f:/home/.emacs.d/inits/")
+          (concat (subseq filename 0 21) "elcs/"
+                  (subseq filename 23) "c")
+          (concat filename "c"))))
+(byte-compile-fdefinition 'byte-compile-maybe-init nil)
+
+(mapc (lambda (file) (load (concat "~/.emacs.d/initelcs/" file)))
+      (cl-remove-if (lambda (x)
+                      (or (string= x ".") (string= x "..")))
+                    (directory-files "~/.emacs.d/initelcs/")))
+(mapc (lambda (file)
+        (unless (file-exists-p (concat "~/.emacs.d/initelcs" file "c"))
+          (load (concat "~/.emacs.d/inits/" file))))
+      (cl-remove-if (lambda (x)
+                      (or (string= x ".") (string= x "..")))
+                    (directory-files "~/.emacs.d/inits/")))
+
+
 
 
 ;;; ------------------------
-;;; Aesthetic customizations
+;;; Aesthetic Customizations
 ;;; ------------------------
 
 (custom-set-variables
@@ -27,19 +56,21 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes nil)
+ '(byte-compile-dest-file-function 'byte-compile-maybe-init)
+ '(company-idle-delay 0.5)
+ '(custom-enabled-themes '(sanityinc-solarized-light sanityinc-solarized-dark))
+ '(custom-safe-themes
+   '("4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" "8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" default))
  '(erc-modules
-   (quote
-    (autojoin button completion dcc fill irccontrols list match menu move-to-prompt netsplit networks noncommands readonly ring stamp track xdcc)))
+   '(autojoin button completion dcc fill irccontrols list match menu move-to-prompt netsplit networks noncommands readonly ring stamp track xdcc))
  '(erc-nick "wohonajax")
  '(erc-nick-uniquifier "_")
  '(erc-user-full-name nil)
  '(face-font-family-alternatives
-   (quote
-    (("Consolas" "Monospace" "fixed")
+   '(("Consolas" "Monospace" "fixed")
      ("Consolas" "CMU Typewriter Text" "fixed")
      ("Sans Serif" "helv" "helvetica" "arial" "fixed")
-     ("helv" "helvetica" "arial" "fixed"))))
+     ("helv" "helvetica" "arial" "fixed")))
  '(js3-auto-indent-p t)
  '(js3-curly-indent-offset 2)
  '(js3-enter-indents-newline t)
@@ -50,33 +81,39 @@
  '(js3-lazy-operators t)
  '(js3-paren-indent-offset 2)
  '(js3-square-indent-offset 2)
- '(quack-default-program "racket"))
+ '(package-selected-packages
+   '(wanderlust rainbow-delimiters slime paredit dropbox flycheck ein py-autopep8 elpy))
+ '(quack-default-program "racket")
+ '(resize-mini-windows t)
+ '(smtpmail-default-smtp-server "smtp.gmail.com")
+ '(smtpmail-smtp-server "smtp.gmail.com")
+ '(smtpmail-smtp-service 587)
+ '(smtpmail-stream-type 'ssl)
+ '(user-full-name "Hunter Chandler")
+ '(user-mail-address "wohonajax@gmail.com")
+ '(warning-suppress-log-types '((server) (server)))
+ '(warning-suppress-types '((server))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#090D0F" :foreground "#CACACA" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 98 :width normal :foundry "outline" :family "Consolas"))))
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "#002b36" :foreground "#839496" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 98 :width normal :foundry "outline" :family "Consolas"))))
  '(dired-symlink ((t (:inherit font-lock-keyword-face :foreground "#B894D1"))))
  '(erc-my-nick-face ((t (:foreground "#B894D1" :weight bold))) t)
  '(erc-notice-face ((t (:foreground "#4F579D" :weight bold))) t)
  '(erc-pal-face ((t (:foreground "medium slate blue" :weight bold))) t)
  '(erc-prompt-face ((t (:foreground "#B894D1" :weight bold))) t)
  '(erc-timestamp-face ((t (:foreground "#65656F" :weight bold))) t)
- '(font-lock-comment-face ((t (:foreground "#D63E34"))))
- '(font-lock-keyword-face ((t (:foreground "#B894D1"))))
+ '(error ((t (:foreground "#FFFF4F" :weight bold))))
+ '(font-lock-comment-face ((t (:foreground "#D66E64"))))
+ '(font-lock-fic-author-face ((t (:background "black" :foreground "orangered" :underline t))))
+ '(font-lock-fic-face ((t (:background "black" :foreground "peru" :weight bold))))
+ '(font-lock-keyword-face ((t (:foreground "#905090"))))
  '(font-lock-string-face ((t (:foreground "#96C83B"))))
  '(font-lock-variable-name-face ((t (:foreground "#53BDB1" :weight bold))))
+ '(font-lock-warning-face ((t (:inherit error))))
  '(highlight-numbers-number ((t (:foreground "#3090B5"))))
- '(link ((t (:foreground "#5FC0E9" :underline t))))
- '(link-visited ((t (:inherit link :foreground "#D98FCA"))))
- '(rainbow-delimiters-depth-1-face ((t (:foreground "#AA88FF"))))
- '(rainbow-delimiters-depth-2-face ((t (:foreground "#FFAA88"))))
- '(rainbow-delimiters-depth-3-face ((t (:foreground "#FFAAFF"))))
- '(rainbow-delimiters-depth-4-face ((t (:foreground "#AAFFFF"))))
- '(rainbow-delimiters-depth-5-face ((t (:foreground "#FFFFAA"))))
- '(rainbow-delimiters-depth-6-face ((t (:foreground "#FFAAAA"))))
- '(rainbow-delimiters-depth-7-face ((t (:foreground "#AAAAFF"))))
- '(rainbow-delimiters-depth-8-face ((t (:foreground "#AAFFAA"))))
- '(rainbow-delimiters-depth-9-face ((t (:foreground "#777777"))))
- '(rainbow-delimiters-unmatched-face ((t (:foreground "#DD2222")))))
+ '(link ((t (:foreground "cornflower blue" :underline t))))
+ '(link-visited ((t (:inherit link :foreground "navy"))))
+ '(show-paren-match ((t (:background "black" :foreground "#9feac5" :inverse-video t)))))
