88cfea0a77e3365aadcc1cced5fc0eed9f56d40d
Hunter Chandler <wohonajax@gmail.com>
2025-08-02 02:00:56 -0500

    Update custom.el

--- custom.el
+++ custom.el
@@ -8,10 +8,14 @@
  '(custom-enabled-themes nil)
  '(custom-file "~/.emacs.d/custom.el")
  '(custom-safe-themes
-   '("18cf5d20a45ea1dff2e2ffd6fbcd15082f9aa9705011a3929e77129a971d1cb3" default))
+   '("09b833239444ac3230f591e35e3c28a4d78f1556b107bafe0eb32b5977204d93"
+     "18cf5d20a45ea1dff2e2ffd6fbcd15082f9aa9705011a3929e77129a971d1cb3"
+     default))
  '(default-frame-alist '((font . "DinaRemasterII-12") (width . 84)))
  '(erc-modules
-   '(autojoin button completion dcc fill irccontrols list match menu move-to-prompt netsplit networks noncommands readonly ring stamp track xdcc))
+   '(autojoin button completion dcc fill irccontrols list match menu
+     move-to-prompt netsplit networks noncommands readonly ring stamp
+     track xdcc))
  '(erc-nick "wohonajax")
  '(erc-nick-uniquifier "1")
  '(erc-user-full-name nil)
@@ -32,8 +36,7 @@
  '(js3-lazy-operators t)
  '(js3-paren-indent-offset 2)
  '(js3-square-indent-offset 2)
- '(package-selected-packages
-   '(sublimity virtualenv fic corfu embark-consult consult which-key orderless marginalia vertico js2-mode xref-js2 fic-mode lsp-mode treemacs neotree sly-asdf magit doom-themes zenburn-theme sly-macrostep helm slime paredit sly rainbow-delimiters dropbox flycheck ein py-autopep8 elpy))
+ '(package-selected-packages nil)
  '(quack-default-program "racket")
  '(read-mail-command 'mu4e)
  '(resize-mini-windows t)
@@ -50,9 +53,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :extend nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight medium :height 120 :width normal :foundry "2ttf" :family "DinaRemasterII"))))
  '(dired-symlink ((t (:inherit nil :foreground "#B894D1"))))
  '(error ((t (:foreground "#FFFF4F" :weight bold))))
  '(font-lock-fic-author-face ((t (:background "black" :foreground "orangered" :underline t))))
  '(font-lock-fic-face ((t (:background "black" :foreground "peru" :weight bold))))
  '(shadow ((t (:foreground "#585659" :slant italic))))
- '(sly-db-frame-label-face ((t nil))))
+ '(sly-db-frame-label-face ((t nil)))
+ '(sly-db-restart-number-face ((t nil))))
