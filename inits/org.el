b8a69e93919d6537296234b880e524e213121fa6
Hunter Chandler <wohonajax@gmail.com>
2025-08-02 01:55:31 -0500

    Delete inits/org.el

--- inits/org.el
+++
@@ -1,9 +0,0 @@
-;;;; -*- lexical-binding: t; -*-
-;;;; ----------------------
-;;;; Org-Mode Customization
-;;;; ----------------------
-
-(require 'org)
-(define-key global-map "\C-cl" 'org-store-link)
-(define-key global-map "\C-ca" 'org-agenda)
-(setq org-log-done t)
